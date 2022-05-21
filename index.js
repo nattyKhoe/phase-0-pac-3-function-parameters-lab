40410c17ac1c25b7e8bfb527682d38fc0d86b7a3
nattyKhoe <natasha.kusuma@outlook.com>
2022-05-21 13:14:51 +1000

    Done with assignment.

---
+++ index.js
@@ -0,0 +1,22 @@
+/*One Parameter*/
+function introduction (name) {
+    return `Hi, my name is ${name}.`;
+}
+/*Two Parameters*/
+
+function logTwovalues (value1, value2){
+    console.log(`The two values are ${value1} and ${value2}`);
+}
+
+function introductionWithLanguage (name,language){
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+}
+
+/*optional parameter*/
+function sayHelloTo (firstName = "User") {
+    console.log (`Hello, ${firstName}!`);
+}
+
+function introductionWithLanguageOptional (name,language = "JavaScript"){
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+}
